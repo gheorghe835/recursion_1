bf16a92afc80250eaae9a066e709ecf082bdc704
GeorgeBatiri <gheorghe835@gmail.com>
2024-04-08 21:36:44 +0300

    added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    //Task 2: Write a programme for calculating the 
+    //Ackerman function using recursion. 
+    //Two non-negative numbers m and n are given.
+    static void Main()
+    {
+        int m = 2; // example of value m
+        int n = 3; // example of value n
+
+        int result = AckermannFunction(m, n);
+        Console.WriteLine($"The result of the Ackerman function  for m = {m} and n = {n} is {result}");
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
+}
